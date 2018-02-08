767a9f9bde3e84bdb9a93e6ead931b8d6178cb6f
Spenca <svatrt@bccrc.ca>
2018-02-08 10:30:34 -0800

    Merge branches and add Windows build

---
+++ Assets/ChangeScene.cs
@@ -0,0 +1,41 @@
+﻿// shout-out to my homie: https://github.com/GraemeClarke
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var sceneIndex = SceneManager.GetActiveScene ().buildIndex;
+		var sceneTotal = SceneManager.sceneCountInBuildSettings;
+
+		if (Input.GetKeyDown ("up")) {
+
+			if (sceneIndex == sceneTotal - 1) {
+				sceneIndex = 0;
+			} else {
+				sceneIndex += 1;
+			}
+
+			SceneManager.LoadScene(sceneIndex);
+		
+		} else if (Input.GetKeyDown ("down")) {
+
+			if (sceneIndex == 0) {
+				sceneIndex = sceneTotal - 1;
+			} else {
+				sceneIndex -= 1;
+			}
+
+			SceneManager.LoadScene(sceneIndex);
+		}
+	}
+}
---
+++ Assets/GenerateTumour2.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System;
+using UnityEngine;
+
+public class GenerateTumour2 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		var input_file = Path.Combine(Directory.GetCurrentDirectory(), "out.csv");
+		var reader = new StreamReader(input_file);
+
+		// Skip lines not pertaining to fields
+		reader.ReadLine();
+
+		while (!reader.EndOfStream) {
+
+			// Start reading at line 2
+			var line = reader.ReadLine();
+			var fields = line.Split(',');
+
+			// For each remaining line in the input file, generate a sphere
+			GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+
+			// Colour the sphere based on the values provided by the input file
+			Color colour = Color.red;
+			if (double.Parse(fields [2]) == 1.0) {
+				colour = Color.green;
+			}
+			sphere.GetComponent<MeshRenderer> ().material.SetColor ("_Color", colour);
+            
+			// Place the sphere based on the values provided by the input file
+            sphere.transform.position = new Vector3(float.Parse(fields [6]), float.Parse(fields [7]), float.Parse(fields [8]));
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
